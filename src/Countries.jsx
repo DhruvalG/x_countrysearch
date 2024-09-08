3a099eca4cab076ca34b86bcf5104e7ea508ffda
DhruvalG <dhruval.gv@gmail.com>
2024-09-08 12:48:03 +0530

    fixed empty page

--- src/Countries.jsx
+++ src/Countries.jsx
@@ -13,7 +13,6 @@ function Country() {
         const search = e.target.value.toLowerCase();
         let name=flags.filter((flag)=> flag.name.toLowerCase().includes(search))
         let flaglist = document.getElementById("mainArea");
-        flaglist.innerHTML = "";
         flaglist.innerHTML = name.map((flag) => `<div className="countryCard" id="countryCard" style=display:flex;flex-Direction:column;justify-Content:center;align-Items:center;padding:10px;margin:10px;border:2px;border-style:solid;border-color:#E1E1E1;border-radius:10px;width:200px;height:200px;>
         <img src=${flag.flag} alt=${flag.abbr} style=width:100px;height:100px;/>
         <h2>${flag.name}</h2>
