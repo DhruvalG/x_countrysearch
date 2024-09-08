1f4cf527cd635637ddd60258b4fcbff65afa7754
DhruvalG <dhruval.gv@gmail.com>
2024-09-08 14:57:43 +0530

    fixed class names

--- src/Countries.jsx
+++ src/Countries.jsx
@@ -10,11 +10,12 @@ function Country() {
         const search = e.target.value.toLowerCase();
         let name=flags.filter((flag)=> flag.name.common.toLowerCase().includes(search))
         let flaglist = document.getElementById("mainArea");
-        flaglist.innerHTML = name.map((flag) => `<div className="countryCard" style=display:flex;flex-Direction:column;justify-Content:center;align-Items:center;padding:10px;margin:10px;border:2px;border-style:solid;border-color:#E1E1E1;border-radius:10px;width:200px;height:200px;>
+        flaglist.innerHTML = name.map((flag) => `<div class='countryCard' style=display:flex;flex-Direction:column;justify-Content:center;align-Items:center;padding:10px;margin:10px;border:2px;border-style:solid;border-color:#E1E1E1;border-radius:10px;width:200px;height:200px;>
         <img src=${flag.flags.png} alt=${flag.altSpellings[0]} style=width:100px;height:100px;/>
         <h2>${flag.name.common}</h2>
     </div>`).slice(",").join("")
     }
+    
     return (
         <>
             <header>
@@ -37,7 +38,7 @@ function Country() {
             }}>
                 {
                     flags.map((flag) => {
-                        return <CountryCard key={flag.altSpellings[0]} name={flag.name.common} flagIMG={flag.flags.png} flagAbbr={flag.altSpellings[0]} /> 
+                        return <CountryCard className=".countryCard" key={flag.altSpellings[0]} name={flag.name.common} flagIMG={flag.flags.png} flagAbbr={flag.altSpellings[0]} /> 
                     })
                 }
             </div>
