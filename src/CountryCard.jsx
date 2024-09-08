dbd66be9dc02c086783b688eb12e2a187ba9c711
DhruvalG <dhruval.gv@gmail.com>
2024-09-08 12:35:43 +0530

    changed div class Name

--- src/CountryCard.jsx
+++ src/CountryCard.jsx
@@ -1,6 +1,6 @@
 let CountryCard = ({name, flagIMG, flagAbbr}) => {
     return (
-        <div style={{display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center",padding:"10px",margin:"10px",border:"2px solid #E1E1E1",borderRadius:"10px",width:"200px",height:"200px"}}>
+        <div className="countryCard" style={{display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center",padding:"10px",margin:"10px",border:"2px solid #E1E1E1",borderRadius:"10px",width:"200px",height:"200px"}}>
             <img src={flagIMG} alt={`Flag of ${flagAbbr}`} style={{
                 width:"100px",
                 height:"100px",
